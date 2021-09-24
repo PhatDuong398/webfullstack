14503182992e43b9dedfea920e00231919a470a9
Dương Lê Tấn Phát <phatduong80@gmail.com>
2021-09-24 22:50:59 +0700

    first commit

---
+++ C_CPP/test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int i=1;
+    do{
+        if(i % 10 == 0){
+            cout<<i<<" ";
+        }
+        i++;
+    }while(i < n);
+    return 0;
+}
